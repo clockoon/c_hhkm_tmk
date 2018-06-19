23ee75e86539cc058d80046d40862babb2b0ea3a
clockoon <clockoon@gmail.com>
2018-06-19 19:41:49 +0900

    adding update batch

--- update.bat
+++ update.bat
@@ -8,20 +8,21 @@ echo *************************
 pause
 
 echo Waiting 5 seconds...
-pause 1
+sleep 1
 echo 4...
-pause 1
+sleep 1
 echo 3...
-pause 1
+sleep 1
 echo 2...
-pause 1
+sleep 1
 echo 1...
-pause 1
+sleep 1
 echo 1) Erasing firmware...
-./dfu-programmer-win/dfu-programmer.exe atmegau32u4 erase
+%~dp0\dfu-programmer-win\dfu-programmer.exe atmega32u4 erase --force
 echo 2) Flashing firmware...
-./dfu-programmer-win/dfu-programmer.exe atmegau32u4 flash c_hkkb_tmk.hex
+%~dp0\dfu-programmer-win\dfu-programmer.exe atmega32u4 flash .\c_hhkb_tmk.hex
 echo 3) Resetting...
-./dfu-programmer-win/dfu-programmer.exe atmegau32u4 reset
+%~dp0\dfu-programmer-win\dfu-programmer.exe atmega32u4 reset
 echo Complete! You now can use updated HHKB
 echo Bye!
+pause
